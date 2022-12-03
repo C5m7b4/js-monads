ed6de6d16f5dc20b960eca5d9b0dcda8f4c35105
C5m7b4 <mbedingfield@dcrpos.com>
2022-12-03 17:27:39 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# js-monads
+
+here we are going to take a look at a few monads
+
+lets create a folder in our programming folder and call it js-monads
+
+bring up the command prompt in the folder and type
+
+```js
+npx kreat
+```
+
+you should see this:
+
+![alt setup](images/01-setup.png)
+
+Im choosing javascript playground for this repo. then just answer the questions for if you want as git repo and if you want to install the dependencies. I would suggest at least for installing the dependencies as you are going to have to do that at some point.
+
+now you can open that project in vscode.
+
+now lets bring up the terminal and type 
+
+```js
+npm start
+```
+
+you should be presented with this in the console after webpack starts up
+
+![alt ready](images/02-ready.png)
---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const EsLintPlugin = require("eslint-webpack-plugin");
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+    port: 3004,
+    compress: true,
+    client: {
+      overlay: false
+    }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Javascript Testing',
+    }),
+    new MiniCssExtractPlugin(),
+	new EsLintPlugin(),
+  ],
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+};
